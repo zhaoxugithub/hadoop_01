8cff1ac11bb61c4a7ce82fb3e72093e84f52a0d1
serenipity <1193131227@qq.com>
2022-09-14 17:22:30 +0800

    提交

---
+++ thread01/src/main/java/thread/c_029_sellticket/TicketSeller3.java
@@ -0,0 +1,43 @@
+package thread.c_029_sellticket;
+
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Copyright (C), 2017-2021, 赵旭
+ * Author: serendipity
+ * Date: 2021/9/12 7:04 下午
+ * FileName: TicketSeller3
+ * Description: com.java.thread.c_029_sellticket
+ */
+public class TicketSeller3 {
+
+    private static LinkedList<String> list  = new LinkedList<>();
+
+    static {
+        for (int i = 0; i < Constants.TICKET_NUMS; i++) {
+            list.add("票号："+i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < Constants.THREAD_COUNT; i++) {
+            new Thread(()->{
+                while(true){
+                    synchronized (list){
+                        if(list.size() <= 0) break;
+
+//                        try {
+//                            TimeUnit.SECONDS.sleep(1);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+                        System.out.println(Thread.currentThread().getName()+"售出了票" + list.remove(0));
+                    }
+
+                }
+            },"t"+i).start();
+        }
+    }
+
+}
